d76666fab452334e62b21e887b2ee3ce1a523a51
abdulqadeer1999 <abdulqadeersolangi007@gmail.com>
2021-03-10 17:59:53 +0500

    cv

--- App.js
+++ App.js
@@ -1,21 +1,33 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View,Image,ScrollView } from 'react-native';
 
 export default function App() {
   return (
+    <ScrollView>
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      
+      <Image source={require('./image/my.jpg')} style={{ width: 100, height: 100 }} />
+      
+      <Text h1>My CV </Text>
+      
+
       <StatusBar style="auto" />
+      
     </View>
+    </ScrollView>
   );
 }
+
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    marginLeft : "20px",
+    marginTop:"20px",
+    
+    alignItems: 'center'
+    // justifyContent: 'center',
   },
 });
